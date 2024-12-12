c4f62db30e867bcb482ed232a0322744fd15ab02
shark <seonjae1003@naver.com>
2024-12-12 16:30:13 +0900

    create slice and use redux state, action

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,35 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    title: "My First Book",
+    price: 6,
+    description: "This is the FIRST book",
+  },
+  {
+    id: "p2",
+    title: "My Second Book",
+    price: 9,
+    description: "This is the Second book",
+  },
+];
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
